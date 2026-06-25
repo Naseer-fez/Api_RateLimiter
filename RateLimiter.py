3d81a3025b4341f580f941d3f7b3983512f31a77
Naseer <feznaseer777@gmail.com>
2026-06-25 16:47:54 +0530

    Fixed Notations

--- RateLimiter.py
+++ RateLimiter.py
@@ -3,9 +3,14 @@ import sys
 __isfileopen :bool = False
 ___samplefile = Path(sys.modules['__main__'].__file__).stem
 def ARL(IP_Adrs:str,Cleaning=False,AutoUpdate=False,
-                CooldownTime=20,AllowedFreq=8,CleaningFreq=80,ResetTime=8,UpdateFreq=8,Filename=___samplefile,FolderPath=None,Format="sql")->int:
-    if Format.lower()=="json":
-        from  ARL import Ratelimiter as Rl
+                CooldownTime=20,AllowedFreq=8,CleaningFreq=80,ResetTime=8,UpdateFreq=8,Filename=___samplefile,FolderPath=None,Format=None)->int:
+    if Format is None:
+        from ARL_nostorage import Ratelimiter as Rl
+        return Rl(IP_Adrs=IP_Adrs,Cleaning=Cleaning,CleaningFreq=CleaningFreq,AllowedFreq=AllowedFreq,ResetTime=ResetTime)
+    elif (isinstance(Format,str) is False):
+        raise TypeError("Only String Option is Availabe for Format Option")
+    elif (Format.lower())=="json":
+        from  ARL_json import Ratelimiter as Rl
         return Rl(
             IP_Adrs=IP_Adrs,Cleaning=Cleaning,AutoUpdate=AutoUpdate,
                 CooldownTime=CooldownTime,AllowedFreq=AllowedFreq,CleaningFreq=CleaningFreq,ResetTime=ResetTime,
@@ -16,6 +21,10 @@ def ARL(IP_Adrs:str,Cleaning=False,AutoUpdate=False,
                 IP_Adrs=IP_Adrs,Cleaning=Cleaning,AutoUpdate=AutoUpdate,
                     CooldownTime=CooldownTime,AllowedFreq=AllowedFreq,CleaningFreq=CleaningFreq,ResetTime=ResetTime,
                     UpdateFreq=UpdateFreq,Filename=Filename,FolderPath=FolderPath)
-    else:
-        from ARL_Nostorage import Ratelimiter as Rl
-        return Rl(IP_Adrs=IP_Adrs,Cleaning=Cleaning,CleaningFreq=CleaningFreq,AllowedFreq=AllowedFreq,ResetTime=ResetTime)
+    else :
+        raise TypeError("""
+        Format Extensions available here  
+        is not valid please provide a valid extension,
+        Availabe : Sql,json,Nostorage
+        Format : json,sql ,(Python (None))
+        """)
